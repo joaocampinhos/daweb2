aa794d2ef528d77c408b2cb5029d6f0b58790bba
joaocampinhos <joao.campinhos@gmail.com>
2013-11-28 19:38:12 +0000

    empezamos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letsgetpro::Application.config.secret_key_base = '2b320c704fb1ef3f893a27d1e33a7a1488e72cea50662af745bb83ee9fcdb2b43c854e86bfad0ec0bfa36162a7b63c890a9aea4eb0e5f00fe222f5e4f83fd526'
