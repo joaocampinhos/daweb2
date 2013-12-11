c8ccfbd574b021b239b087d748eecafc8fcd4080
joaocampinhos <joao.campinhos@gmail.com>
2013-12-11 22:09:14 +0000

    search e cenas

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,20 @@ class SearchController < ApplicationController
   def index
     #Pesquisa
     #Retirar argumentos do URL, pesquisar e "mandar" para a view
+    #find(:all, :conditions => ['name LIKE ?', "%#{search}%"]
+    @empresas = []
+    @empregados = []
+    logger.debug params
+    if params[:q]
+      search = params[:q].downcase
+      @empresas = Empresa.find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{search}%"])
+      @empregados= Empregado.find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{search}%"])
+    elsif params[:contact]
+      search = params[:contact].downcase
+      @empregados= Empregado.find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{search}%"])
+    elsif params[:company]
+      search = params[:company].downcase
+      @empresas = Empresa.find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{search}%"])
+    end
   end
 end
